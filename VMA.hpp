d1d838358d4dfe6343e7bc84716566eb6cb3f65a
Ptah <m.moinvaziri@gmail.com>
2022-06-29 22:26:23 +0200

    [Update] Linux

--- VMA.hpp
+++ VMA.hpp
@@ -1,19 +1,13 @@
 #pragma once
 
-#include <Kube/Core/Platform.hpp>
-
 #define VMA_VULKAN_VERSION 1002000
 
-#if KUBE_COMPILER_GCC
-# pragma GCC diagnostic push
-# pragma GCC diagnostic ignored "-Wold-style-cast"
-# pragma GCC diagnostic ignored "-Wunused-parameter"
-# pragma GCC diagnostic ignored "-Wconversion"
-# pragma GCC diagnostic ignored "-Wtype-limits"
-# pragma GCC diagnostic ignored "-Wunused-variable"
-# pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-# include <vk_mem_alloc.h>
-# pragma GCC diagnostic pop
-#else
-# include <vk_mem_alloc.h>
-#endif
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wparentheses"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wconversion"
+#include <vk_mem_alloc.h>
+#pragma GCC diagnostic pop
